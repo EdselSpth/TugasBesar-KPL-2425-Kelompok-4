87f2408883301f86e4dab9b9ab0d6adb30a78885
edselspth <edselspth@gmail.com>
2025-04-23 10:41:10 +0700

    Feat: [ESH] Menambahkan Implementasi Awal Nama Kelompok (23 April 2025)

--- TugasBesar-KPL-2425-Kelompok-4/Program.cs
+++ TugasBesar-KPL-2425-Kelompok-4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TugasBesar_KPL_2425_Kelompok_4
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ReGreen");
+            Console.WriteLine("Kelompok 4");
+            Console.WriteLine("Edsel Septa Haryanto - 103022300016");
+            Console.WriteLine("Gusti Agung Raka Darma Putra Kepakisan - 103022300088");
+            Console.WriteLine("Deru Khairan Djuharianto - 103022300101");
+            Console.WriteLine("Reza Indra Maulana - 103022300109");
+            Console.WriteLine("Abdul Azis Saepurohmat - 103022300092");
+            Console.WriteLine("Tio Funny Tinambunan - 103022330036");
+            Console.WriteLine("=====================================\n");
+        }
+    }
+}
