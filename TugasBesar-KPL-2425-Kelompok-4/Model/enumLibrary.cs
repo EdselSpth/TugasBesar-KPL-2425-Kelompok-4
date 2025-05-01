aa376c6c45072b87b6f5957980e52f605f7b2438
edselspth <edselspth@gmail.com>
2025-05-01 20:44:37 +0700

    feat: [ESH] Membuat model pengguna, jadwal, serta enumLibrary (1 April 2025)

---
+++ TugasBesar-KPL-2425-Kelompok-4/Model/enumLibrary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasBesar_KPL_2425_Kelompok_4.Model
+{
+    public enum JenisSampah
+    {
+        Organik,
+        Plastik,
+        Kertas,
+        Logam,
+        Elektronik,
+        BahanBerbahaya,
+        Minyak,
+    }
+
+    public enum JenisPengguna
+    {
+        Admin,
+        Kurir,
+        Pengguna,
+    }
+}
