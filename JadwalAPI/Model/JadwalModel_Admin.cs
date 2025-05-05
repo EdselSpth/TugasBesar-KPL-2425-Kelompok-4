4ac2018b196451b9a2d227b2d88ea5af02734fa8
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-05-05 08:44:26 +0700

    add : [GAR] menambahkan jadwal API untuk admin (5 Mei 2025)

---
+++ JadwalAPI/Model/JadwalModel_Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace JadwalAPI.Model
+{
+    public class JadwalModel_Admin
+    {
+        public DateOnly Tanggal { get; set; }
+        public List<string> JenisSampah { get; set; }
+        public string namaKurir { get; set; }
+    }
+}
