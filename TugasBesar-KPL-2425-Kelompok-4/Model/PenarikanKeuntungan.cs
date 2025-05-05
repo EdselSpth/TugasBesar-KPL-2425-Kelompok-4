2aff339097cc476eaac7a47f3340c224fe8bd56b
tifunnyy <funnyrnd92@gmail.com>
2025-05-05 21:31:07 +0700

    Penarikan keuntungan

---
+++ TugasBesar-KPL-2425-Kelompok-4/Model/PenarikanKeuntungan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasBesar_KPL_2425_Kelompok_4.Model
+{
+    internal class PenarikanKeuntungan
+    {
+        public static bool VerifikasiRekening(string rekening)
+        {
+            return rekening.Length == 12;
+        }
+
+        private static Dictionary<JenisPengguna, Action> tindakanPenarikan = new Dictionary<JenisPengguna, Action>
+        {
+            { JenisPengguna.Pengguna, () => Console.WriteLine("Pengguna dapat memasukkan nomor rekening untuk mengambil keuntungan.") },
+            { JenisPengguna.Admin, () => Console.WriteLine("Admin dapat menyetujui permintaan dan melakukan transfer ke nomor rekening.") }
+            };
+
+            public static void MelakukanPenarikan(JenisPengguna pengguna)
+            {
+                if (tindakanPenarikan.ContainsKey(pengguna))
+                {
+                    tindakanPenarikan[pengguna].Invoke();
+                }
+                else
+                {
+                    Console.WriteLine("Tindakan tidak tersedia untuk jenis pengguna ini.");
+                }
+            }
+        }
+    }
