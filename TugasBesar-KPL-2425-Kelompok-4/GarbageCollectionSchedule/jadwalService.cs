89395d64d9b1438fff3afb773a68241dd5b2ab59
edselspth <edselspth@gmail.com>
2025-05-06 15:35:59 +0700

    fix: [ESH] menyelesaikan merging error (05 April 2025

---
+++ TugasBesar-KPL-2425-Kelompok-4/GarbageCollectionSchedule/jadwalService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasBesar_KPL_2425_Kelompok_4.GarbageCollectionSchedule
+{
+    class jadwalService
+    {
+        public void showSchedule() { 
+        
+        }
+    }
+}
