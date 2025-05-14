d4d1fe094b91ce97eb5bded15b86f6ac726dbf9b
edselspth <edselspth@gmail.com>
2025-05-14 12:32:03 +0700

    fix: [ESH] Menyesuaikan output client (14 Mei 2025)

--- TugasBesar-KPL-2425-Kelompok-4/Menu.cs
+++ TugasBesar-KPL-2425-Kelompok-4/Menu.cs
@@ -8,35 +8,40 @@ namespace TugasBesar_KPL_2425_Kelompok_4
 {
     class Menu
     {
-        public static void menuAdmin(string username)
+        public static void menuAdmin()
         {
-            Console.WriteLine("Selamat Datang, " + username);
+            Console.WriteLine("∘<<──────>>∘ Menu Admin ∘<<──────>>∘");
             Console.WriteLine("(1) Menjadwalkan pengambilan sampah");
             Console.WriteLine("(2) Melihat jadwal pengambilan sampah");
             Console.WriteLine("(3) Mengubah jadwal pengambilan sampah");
             Console.WriteLine("(4) Menghapus jadwal pengambilan sampah");
             Console.WriteLine("(5) Menyetujui penarikan keuntungan");
             Console.WriteLine("(6) Keluar");
-            Console.WriteLine("Pilih Menu : ");
+            Console.WriteLine("∘<<──────>>∘∘<<──────>>∘∘<<──────>>∘");
         }
 
-        public static void menuUser(string username)
+        public static void menuUser()
         {
-            Console.WriteLine("Selamat Datang, " + username);
+            Console.WriteLine("∘<<──────>>∘ Menu Pengguna ∘<<──────>>∘");
             Console.WriteLine("(1) Melihat jadwal pengambilan sampah");
             Console.WriteLine("(2) Menarik keuntungan dari sampah");
             Console.WriteLine("(4) Mendaftarkan area pengambilan sampah");
-            Console.WriteLine("(4) Mendaftarkan pengambilan sampah");
-            Console.WriteLine("(5) Keluar");
-            Console.WriteLine("Pilih Menu : ");
+            Console.WriteLine("(5) Mendaftarkan pengambilan sampah");
+            Console.WriteLine("(6) Keluar");
+            Console.WriteLine("∘<<──────>>∘∘<<──────>>∘∘<<──────>>∘");
         }
 
-        public static void menuKurir(string username)
+        public static void menuKurir()
         {
-            Console.WriteLine("Selamat Datang, " + username);
+            Console.WriteLine("∘<<──────>>∘ Menu Kurir ∘<<──────>>∘");
             Console.WriteLine("(1) Melihat jadwal pengambilan sampah");
             Console.WriteLine("(2) Keluar");
-            Console.WriteLine("Pilih Menu : ");
+            Console.WriteLine("∘<<──────>>∘∘<<──────>>∘∘<<──────>>∘");
+        }
+        
+        public static void header()
+        {
+            Console.WriteLine("∘<<──────>>∘∘<<──────>>∘∘<<──────>>∘");
         }
     }
 }
